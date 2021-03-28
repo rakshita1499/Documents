f66e076194ccba280b8929a8521d9db3d8d41fdc
rakshita <rakshitahippargi@gmail.com>
2021-03-28 15:58:48 +0530

    first commit

---
+++ tps2.py
@@ -0,0 +1,23 @@
+class Parent:
+    students = dict()
+    try:
+        n =input("Enter Number of student:")
+        if  n.isalnum()==True:
+         except ValueError:
+         print("valueerror")
+        elif:
+         n.isalpha==True
+          except TypeError:
+          print("typeerror")
+    try:
+        n.isdecimal==True
+    except TypeError:
+        print("type error")
+    except KeyboardInterrupt:
+        print("keyboard interrupt")
+    except IndentationError:
+        print("indentation error")
+    except IndexError:
+        print("index error")
+    def getNumberOfEnterStudent(self):
+        return self.n
